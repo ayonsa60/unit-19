cf69c83689c26be6c98d26ea310edc312485905a
ayonsa60 <ayonsa60@gmail.com>
2021-11-30 22:18:51 -0600

    added files to cache

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,1 +1,16 @@
-console.log('we found your file');
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-512x512.png',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/service-worker.js',
+    '/server.js',
+    '/routes/api.js',
+    '/models/transactions.js',
+
+]
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
